9005d986f9af3fe1c0966acbffa363287908c523
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-07-06 11:26:26 +0430

    comment for debug

--- run.py
+++ run.py
@@ -471,9 +471,9 @@ def main():
     print("start loading model")
     model = model_fn.from_pretrained(
         model_args.model_name_or_path,
-        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        #from_tf=bool(".ckpt" in model_args.model_name_or_path),
         config=config,
-        cache_dir=model_args.cache_dir,
+        #cache_dir=model_args.cache_dir,
     )
     print("model loaded")
     # For BERT, increase the size of the segment (token type) embeddings
