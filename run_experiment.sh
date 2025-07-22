301a67d24cfa239e3cefd16c1359714142bf71b1
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-07-22 16:40:09 +0430

    edit sh file

--- run_experiment.sh
+++ run_experiment.sh
@@ -96,7 +96,7 @@ case $TASK in
     farstail)
       TEMPLATE=*cls**sent_0*؟_*mask*،_*sent_1**sep+*
       MAPPING="{'e':'بله','c':'خیر','n':'شاید'}"
-      TASK_EXTRA="--max_seq_len 512 --num_sample 16"
+      TASK_EXTRA="--max_seq_len 512 --num_sample 16 --demo_filter_model /kaggle/working/all_minilm/bert-base-parsbert-uncased --demo_filter"
       ;;
     miras)
       TEMPLATE=*cls*mask*_بود*sent_0*sep+*
