cfb615cbaea3d6eb1fd33236d178820d14847631
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-07-23 22:07:54 +0430

    miras get label

--- tools/run_generate_labels.sh
+++ tools/run_generate_labels.sh
@@ -29,7 +29,7 @@ K_NEIGHBORS=30
 # How many label mappings per template to keep at the end.
 N_PAIRS=100
 
-TASKS="farstail"
+TASKS="miras"
 
 SEEDS="100 13 21 42 87"
 
@@ -110,6 +110,10 @@ for TASK in $TASKS; do
             farstail)
                 TEMPLATE=*cls**sent_0*؟_*mask*،_*sent_1**sep+*
                 MAPPING="{'e':'بله','c':'خیر','n':'شاید'}"
+                ;;
+            miras)
+                TEMPLATE=*cls*mask*_بود*sent_0*sep+*
+                MAPPING="{0:'بد',2:'متوسط',1:'خوب'}"
                 ;;
         esac
 
