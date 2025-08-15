93411dd01c4b70ac4cf2b5aaedb8aeccc31e5ddf
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-08-15 18:02:48 +0430

    fix

--- tools/generate_labels.py
+++ tools/generate_labels.py
@@ -292,7 +292,7 @@ def main():
     print(f"label pairing is {label_pairings}")
     with open(data_args.output_file, mode, encoding="utf-8") as f:
         for pairing in label_pairings:
-            words = [vocab[i][len(special_token):] for i in pairing]
+            words = [tokenizer.convert_tokens_to_string(vocab[i][len(special_token):]) for i in pairing]
             print(f"words is {words}")
             mapping = {labels[i]: words[i] for i in range(len(labels))}
             if data_args.write_template:
