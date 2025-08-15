c91efc63be7e303e945cc5c6b62714a3f74b8e1b
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-08-15 19:05:46 +0430

    change random seg

--- tools/generate_labels.py
+++ tools/generate_labels.py
@@ -226,7 +226,7 @@ def main():
     # For BERT, increase the size of the segment (token type) embeddings
     if config.model_type == 'bert':
         model.resize_token_embeddings(len(tokenizer))
-        resize_token_type_embeddings(model, new_num_types=10, random_segment=False) # this line change
+        resize_token_type_embeddings(model, new_num_types=10, random_segment=True) # this line change
         special_token = "##"
 
     # Pass dataset and argument information to the model
