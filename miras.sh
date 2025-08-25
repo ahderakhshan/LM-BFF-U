f17f4af0103c8d349cfe425f27118d4ce50ccb03
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-08-26 01:46:30 +0430

    change to roberta

--- miras.sh
+++ miras.sh
@@ -12,7 +12,7 @@ do
             BS=$bs \
             LR=$lr \
             SEED=$seed \
-            MODEL=/kaggle/working/all_minilm/bert-base-parsbert-uncased \
+            MODEL=/kaggle/working/all_minilm/roberta-fa-zwnj-base \
             bash run_experiment.sh
         done
     done
