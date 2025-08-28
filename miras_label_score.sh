e4a2f6a65cdab3d964d6ba9d63f57d37191c2890
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-08-28 14:51:29 +0430

    miras auto label

---
+++ miras_label_score.sh
@@ -0,0 +1,19 @@
+for mapping_id in {0..99}
+do
+    for seed in 13 21 42 87 100
+    do
+        # To save time, we fix these hyper-parameters
+        bs=8
+        lr=1e-5
+
+        # Since we only use dev performance here, use --no_predict to skip testing
+        TAG=exp-mapping \
+        TYPE=prompt \
+        TASK=miras-sparrow \
+        BS=$bs \
+        LR=$lr \
+        SEED=$seed \
+        MODEL=/kaggle/working/all_minilm/roberta-fa-zwnj-base \
+        bash run_experiment.sh "--mapping_path me_auto_label_mapping/manual_template/miras-sparrow/16-$seed.txt --mapping_id $mapping_id --no_predict"
+    done
+done
