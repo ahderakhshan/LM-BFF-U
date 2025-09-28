96eb743308ea7d13f00fd0bb9d2539d5ef2e5531
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-09-28 20:05:37 +0330

    fix

--- src/trainer.py
+++ src/trainer.py
@@ -472,7 +472,7 @@ class Trainer(transformers.Trainer):
         eval_dataloader = self.get_eval_dataloader(eval_dataset)
         print("eval dataloader info")
         print(len(eval_dataloader))
-        print(eval_dataset.query_examples)
+        print(eval_dataset.query_examples[0].text_a)
         print(type(eval_dataset))
 
         output = self.prediction_loop(eval_dataloader, description="Evaluation")
