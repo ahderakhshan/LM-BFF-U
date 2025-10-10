35847b7694e651dfa00f78946bc12040283d71bf
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-10-10 17:29:43 +0330

    fix log

--- run.py
+++ run.py
@@ -634,6 +634,8 @@ def main():
             final_result.update(vars(model_args))
             final_result.update(vars(training_args))
             final_result.update(vars(data_args))
+            if 'distributed_state' in final_result:
+                final_result.pop('distributed_state')
             if 'evaluation_strategy' in final_result:
                 final_result.pop('evaluation_strategy')
             f.write(str(final_result) + '\n')
