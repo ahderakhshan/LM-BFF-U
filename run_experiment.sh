0cf282733921b4597ea778f411434dddeb48bd22
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2025-11-25 23:08:48 +0330

    debug

--- run_experiment.sh
+++ run_experiment.sh
@@ -110,7 +110,7 @@ case $TASK in
       ;;
     parsinlu-food-sentiment)
       TEMPLATE=*cls*sent_0*mask*_*sep+*
-      MAPPING="{'Positive':'خوب','Negative':'بد','Neutral':'متوسط'}"
+      MAPPING="{'Positive':'خوب','Negative':'خوب','Neutral':'خوب'}"
       TASK_EXTRA="--max_seq_len 512 --num_sample 16"
 esac
 
