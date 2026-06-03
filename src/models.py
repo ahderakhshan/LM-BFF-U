6e889fbeda50a592d64f2f571e2150630499becf
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-03 15:43:21 +0430

    debug

--- src/models.py
+++ src/models.py
@@ -241,64 +241,64 @@ class XLMRobertaForPromptFinetuning(XLMRobertaPreTrainedModel):
         # For auto label search.
         self.return_full_softmax = None
 
-        def forward(
-                self,
-                input_ids=None,
-                attention_mask=None,
-                mask_pos=None,
-                labels=None,
-        ):
-            batch_size = input_ids.size(0)
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            mask_pos=None,
+            labels=None,
+    ):
+        batch_size = input_ids.size(0)
 
-            if mask_pos is not None:
-                mask_pos = mask_pos.squeeze()
+        if mask_pos is not None:
+            mask_pos = mask_pos.squeeze()
 
-            # Encode everything
-            outputs = self.roberta(
-                input_ids,
-                attention_mask=attention_mask
-            )
+        # Encode everything
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask
+        )
 
-            # Get <mask> token representation
-            sequence_output, pooled_output = outputs[:2]
-            sequence_mask_output = sequence_output[torch.arange(sequence_output.size(0)), mask_pos]
+        # Get <mask> token representation
+        sequence_output, pooled_output = outputs[:2]
+        sequence_mask_output = sequence_output[torch.arange(sequence_output.size(0)), mask_pos]
 
-            # Logits over vocabulary tokens
-            prediction_mask_scores = self.lm_head(sequence_mask_output)
+        # Logits over vocabulary tokens
+        prediction_mask_scores = self.lm_head(sequence_mask_output)
 
-            # Exit early and only return mask logits.
-            if self.return_full_softmax:
-                if labels is not None:
-                    return torch.zeros(1, out=prediction_mask_scores.new()), prediction_mask_scores
-                return prediction_mask_scores
-
-            # Return logits for each label
-            logits = []
-            for label_id in range(len(self.label_word_list)):
-                logits.append(prediction_mask_scores[:, self.label_word_list[label_id]].unsqueeze(-1))
-            logits = torch.cat(logits, -1)
-
-            # Regression task
-            if self.config.num_labels == 1:
-                logsoftmax = nn.LogSoftmax(-1)
-                logits = logsoftmax(logits)  # Log prob of right polarity
-
-            loss = None
+        # Exit early and only return mask logits.
+        if self.return_full_softmax:
             if labels is not None:
-                if self.num_labels == 1:
-                    # Regression task
-                    loss_fct = nn.KLDivLoss(log_target=True)
-                    labels = torch.stack([1 - (labels.view(-1) - self.lb) / (self.ub - self.lb),
-                                          (labels.view(-1) - self.lb) / (self.ub - self.lb)], -1)
-                    loss = loss_fct(logits.view(-1, 2), labels)
-                else:
-                    loss_fct = nn.CrossEntropyLoss()
-                    loss = loss_fct(logits.view(-1, logits.size(-1)), labels.view(-1))
+                return torch.zeros(1, out=prediction_mask_scores.new()), prediction_mask_scores
+            return prediction_mask_scores
 
-            output = (logits,)
+        # Return logits for each label
+        logits = []
+        for label_id in range(len(self.label_word_list)):
+            logits.append(prediction_mask_scores[:, self.label_word_list[label_id]].unsqueeze(-1))
+        logits = torch.cat(logits, -1)
+
+        # Regression task
+        if self.config.num_labels == 1:
+            logsoftmax = nn.LogSoftmax(-1)
+            logits = logsoftmax(logits)  # Log prob of right polarity
+
+        loss = None
+        if labels is not None:
             if self.num_labels == 1:
-                # Regression output
-                output = (torch.exp(logits[..., 1].unsqueeze(-1)) * (self.ub - self.lb) + self.lb,)
-            return ((loss,) + output) if loss is not None else output
+                # Regression task
+                loss_fct = nn.KLDivLoss(log_target=True)
+                labels = torch.stack([1 - (labels.view(-1) - self.lb) / (self.ub - self.lb),
+                                      (labels.view(-1) - self.lb) / (self.ub - self.lb)], -1)
+                loss = loss_fct(logits.view(-1, 2), labels)
+            else:
+                loss_fct = nn.CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, logits.size(-1)), labels.view(-1))
+
+        output = (logits,)
+        if self.num_labels == 1:
+            # Regression output
+            output = (torch.exp(logits[..., 1].unsqueeze(-1)) * (self.ub - self.lb) + self.lb,)
+        return ((loss,) + output) if loss is not None else output
 
 
