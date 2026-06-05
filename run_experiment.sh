deddfd956c8eedc8f612a10f77b5510380383209
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-05 16:37:53 +0430

    change mapping

--- run_experiment.sh
+++ run_experiment.sh
@@ -115,7 +115,7 @@ case $TASK in
       ;;
     farexstance)
       TEMPLATE=*cls**sent_1**mask*با*sent_0*است.**sep+*
-      MAPPING="{'agree':'موافق','disagree':'مخالف','unrelated':'بیربط','related':'مرتبط'}"
+      MAPPING="{'agree':'موافق','disagree':'مخالف','unrelated':'ناربط','related':'مرتبط'}"
       TASK_EXTRA="--max_seq_len 512 --num_sample 16"
       ;;
 esac
