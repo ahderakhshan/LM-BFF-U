8d591857d69f065b4952491babf7072fa05db063
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-05 21:58:20 +0430

    train with filter files

---
+++ task_train_demo_filter.sh
@@ -0,0 +1,20 @@
+for task in miras-sparrow farstail farexstance
+do
+  for seed in 13
+  do
+      for bs in 8
+      do
+          for lr in 5e-5
+          do
+              TAG="${task}_train_demo-filter" \
+              TYPE=prompt-demo \
+              TASK="${task}" \
+              BS=$bs \
+              LR=$lr \
+              SEED=$seed \
+              MODEL=FacebookAI/xlm-roberta-large \
+              bash run_experiment_train_demo-filter.sh
+          done
+      done
+  done
+done
