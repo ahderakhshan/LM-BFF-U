43779c63c5dd05a2b93060b659ea8f6d7722b78a
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-05 16:55:32 +0430

    change template

--- run_experiment.sh
+++ run_experiment.sh
@@ -114,7 +114,7 @@ case $TASK in
       TASK_EXTRA="--max_seq_len 512 --num_sample 16"
       ;;
     farexstance)
-      TEMPLATE=*cls**sent_1**mask**sent_0*است.**sep+*
+      TEMPLATE=*cls**sent_1**mask*با*sent_0*است.**sep+*
       MAPPING="{'agree':'موافق','disagree':'مخالف','unrelated':'جدا','discuss':'مرتبط'}"
       TASK_EXTRA="--max_seq_len 512 --num_sample 16"
       ;;
