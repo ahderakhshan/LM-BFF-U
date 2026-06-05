5002e4c37a5d0116a9b9266837a6e45543f0c5b6
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-05 15:44:37 +0430

    debug

--- run_experiment.sh
+++ run_experiment.sh
@@ -94,7 +94,7 @@ case $TASK in
         TASK_EXTRA="--first_sent_limit 110  --double_demo"
         ;;
     farstail)
-      TEMPLATE=*cls**sent_0*؟*mask*،*sent_1**sep+*
+      TEMPLATE=*cls**sent_0*؟_*sent_1**sep+*
       MAPPING="{'e':'بله','c':'خیر','n':'شاید'}"
       TASK_EXTRA="--max_seq_len 512 --num_sample 16 --demo_filter_model /kaggle/working/all_minilm/roberta-fa-zwnj-base --demo_filter"
       ;;
