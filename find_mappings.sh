ce91f5ce5d631008e479a44ccda1c814f260186b
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-23 23:20:26 +0430

    full find mappings for miras-sparrow

---
+++ find_mappings.sh
@@ -0,0 +1,19 @@
+for mapping_id in {0..99}
+do
+    for seed in 13 21 42 87 100
+    do
+        # To save time, we fix these hyper-parameters
+        bs=8
+        lr=1e-5
+
+        # Since we only use dev performance here, use --no_predict to skip testing
+        TAG=exp-mapping \
+        TYPE=prompt \
+        TASK=miras-sparrow \
+        BS=$bs \
+        LR=$lr \
+        SEED=$seed \
+        MODEL=xlm-roberta-large \
+        bash run_experiments_find_mappings.sh "--mapping_path my_auto_label_mapping/manual_template/$TASK/16-$seed.txt --mapping_id $mapping_id --no_predict"
+    done
+done
