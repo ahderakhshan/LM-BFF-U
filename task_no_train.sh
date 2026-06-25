289dc613a9478fa2b34eade2a8509a387ecb53af
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-26 00:37:03 +0430

    fix

--- task_no_train.sh
+++ task_no_train.sh
@@ -1,7 +1,7 @@
 
 for task in miras-sparrow
 do
-  for seed in 13
+  for seed in 13 21 42 87 100
   do
       for bs in 8
       do
@@ -14,7 +14,7 @@ do
               LR=$lr \
               SEED=$seed \
               MODEL=FacebookAI/xlm-roberta-large \
-              bash run_experiment_no_train.sh "--mapping_path final_label_mapping/miras-sparrow/16-$seed.txt --mapping_id 0"
+              bash run_experiment_no_train.sh "--mapping_path final_label_mapping/miras-sparrow/16-$seed.sort.txt --mapping_id 0"
           done
       done
   done
