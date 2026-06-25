54655425e0e09a0b89a60ef3f9b3dd20afeddad4
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-26 00:39:54 +0430

    limit exp

--- task_no_train.sh
+++ task_no_train.sh
@@ -21,23 +21,23 @@ do
 done
 
 
-for task in miras-sparrow farstail farexstance
-do
-  for seed in 13
-  do
-      for bs in 8
-      do
-          for lr in 5e-5
-          do
-              TAG="${task}_no-train_demo" \
-              TYPE=prompt-demo \
-              TASK="${task}" \
-              BS=$bs \
-              LR=$lr \
-              SEED=$seed \
-              MODEL=FacebookAI/xlm-roberta-large \
-              bash run_experiment_no_train.sh
-          done
-      done
-  done
-done
+#for task in miras-sparrow farstail farexstance
+#do
+#  for seed in 13
+#  do
+#      for bs in 8
+#      do
+#          for lr in 5e-5
+#          do
+#              TAG="${task}_no-train_demo" \
+#              TYPE=prompt-demo \
+#              TASK="${task}" \
+#              BS=$bs \
+#              LR=$lr \
+#              SEED=$seed \
+#              MODEL=FacebookAI/xlm-roberta-large \
+#              bash run_experiment_no_train.sh
+#          done
+#      done
+#  done
+#done
