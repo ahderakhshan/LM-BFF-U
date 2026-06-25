09e5ae20157f042a2dff8e8cf435116c18f7b89c
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-26 01:49:24 +0430

    run exps

--- task_no_train.sh
+++ task_no_train.sh
@@ -1,36 +1,14 @@
 
-for task in miras-sparrow
-do
-  for seed in 13 21 42 87 100
-  do
-      for bs in 8
-      do
-          for lr in 5e-5
-          do
-              TAG="${task}_no-train_no-demo_manuallabel" \
-              TYPE=prompt \
-              TASK="${task}" \
-              BS=$bs \
-              LR=$lr \
-              SEED=$seed \
-              MODEL=FacebookAI/xlm-roberta-large \
-              bash run_experiment_no_train.sh
-          done
-      done
-  done
-done
-
-
-#for task in miras-sparrow farstail farexstance
+#for task in miras-sparrow
 #do
-#  for seed in 13
+#  for seed in 13 21 42 87 100
 #  do
 #      for bs in 8
 #      do
 #          for lr in 5e-5
 #          do
-#              TAG="${task}_no-train_demo" \
-#              TYPE=prompt-demo \
+#              TAG="${task}_no-train_no-demo_manuallabel" \
+#              TYPE=prompt \
 #              TASK="${task}" \
 #              BS=$bs \
 #              LR=$lr \
@@ -41,3 +19,25 @@ done
 #      done
 #  done
 #done
+
+
+for task in miras-sparrow
+do
+  for seed in 13 21 42 87 100
+  do
+      for bs in 8
+      do
+          for lr in 5e-5
+          do
+              TAG="${task}_no-train_demo_manuallabel" \
+              TYPE=prompt-demo \
+              TASK="${task}" \
+              BS=$bs \
+              LR=$lr \
+              SEED=$seed \
+              MODEL=FacebookAI/xlm-roberta-large \
+              bash run_experiment_no_train.sh
+          done
+      done
+  done
+done
