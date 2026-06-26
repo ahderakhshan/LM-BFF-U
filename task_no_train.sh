e1dd3609588690526aef844f2cfe53302b498302
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-26 12:03:44 +0430

    mapping selection

--- task_no_train.sh
+++ task_no_train.sh
@@ -1,4 +1,26 @@
 
+for task in miras-sparrow
+do
+  for seed in 13
+  do
+      for bs in 8
+      do
+          for lr in 5e-5
+          do
+              TAG="${task}_no-train_no-demo_mapping-selection" \
+              TYPE=prompt \
+              TASK="${task}" \
+              BS=$bs \
+              LR=$lr \
+              SEED=$seed \
+              MODEL=FacebookAI/xlm-roberta-large \
+              bash run_experiment_no_train.sh
+          done
+      done
+  done
+done
+
+
 #for task in miras-sparrow
 #do
 #  for seed in 13 21 42 87 100
@@ -7,37 +29,15 @@
 #      do
 #          for lr in 5e-5
 #          do
-#              TAG="${task}_no-train_no-demo_manuallabel" \
-#              TYPE=prompt \
+#              TAG="${task}_no-train_demo_autolabel" \
+#              TYPE=prompt-demo \
 #              TASK="${task}" \
 #              BS=$bs \
 #              LR=$lr \
 #              SEED=$seed \
 #              MODEL=FacebookAI/xlm-roberta-large \
-#              bash run_experiment_no_train.sh
+#              bash run_experiment_no_train.sh "--mapping_path final_label_mapping/miras-sparrow/16-$seed.sort.txt --mapping_id 0"
 #          done
 #      done
 #  done
 #done
-
-
-for task in miras-sparrow
-do
-  for seed in 13 21 42 87 100
-  do
-      for bs in 8
-      do
-          for lr in 5e-5
-          do
-              TAG="${task}_no-train_demo_autolabel" \
-              TYPE=prompt-demo \
-              TASK="${task}" \
-              BS=$bs \
-              LR=$lr \
-              SEED=$seed \
-              MODEL=FacebookAI/xlm-roberta-large \
-              bash run_experiment_no_train.sh "--mapping_path final_label_mapping/miras-sparrow/16-$seed.sort.txt --mapping_id 0"
-          done
-      done
-  done
-done
