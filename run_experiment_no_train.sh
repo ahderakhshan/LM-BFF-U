5b2916e8d8930fa010f5b40365c4711048315633
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-06-27 21:18:41 +0430

    run exps

--- run_experiment_no_train.sh
+++ run_experiment_no_train.sh
@@ -94,9 +94,9 @@ case $TASK in
         TASK_EXTRA="--first_sent_limit 110  --double_demo"
         ;;
     farstail)
-      TEMPLATE=*cls**sent_0**mask*،*sent_1**sep+*
+      TEMPLATE=*cls**sent_0*،*sent_1*؟*mask**sep+*
       MAPPING="{'e':'بله','c':'خیر','n':'شاید'}"
-      TASK_EXTRA="--max_seq_len 512 --num_sample 16"
+      TASK_EXTRA="--max_seq_len 512 --num_sample 16 --del_a_last_char --del_b_last_char"
       ;;
     miras)
       TEMPLATE=*cls*mask*_بود*sent_0*sep+*
