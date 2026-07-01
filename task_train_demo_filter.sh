e470a8ad7c164fae833c6659d5e12fe6734c4783
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-07-02 00:21:03 +0430

    run exps for demo filter

--- task_train_demo_filter.sh
+++ task_train_demo_filter.sh
@@ -1,10 +1,20 @@
-for task in miras-sparrow farstail farexstance
+python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task miras-sparrow
+python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task miras-sparrow --do_test --seed 42
+for seed in 13 21 87 100
+do
+    for task in miras-sparrow
+    do
+        cp data/k-shot/$task/16-42/test.npy  data/k-shot/$task/16-$seed/
+    done
+done
+counter=0
+for task in miras-sparrow
 do
   for seed in 13 21 42 87 100
   do
-      for bs in 8
+      for bs in 2 4 8
       do
-          for lr in 5e-5
+          for lr in 1e-5 2e-5 5e-5
           do
               TAG="${task}_train_demo-filter" \
               TYPE=prompt-demo \
@@ -14,6 +24,11 @@ do
               SEED=$seed \
               MODEL=FacebookAI/xlm-roberta-large \
               bash run_experiment_train_demo-filter.sh
+              sleep 120s
+              ((counter++))
+              if (( counter % 10 == 0 )); then
+                sleep 600s
+              fi
           done
       done
   done
