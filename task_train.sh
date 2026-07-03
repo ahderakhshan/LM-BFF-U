52086cacb25565f0ac3074e635324308fb8192cf
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-07-03 12:39:29 +0430

    full lr farstail exps

--- task_train.sh
+++ task_train.sh
@@ -1,32 +1,32 @@
 #
-#for task in farstail
-#do
-#  for seed in 13 21 42 87 100
-#  do
-#      for bs in 8
-#      do
-#          for lr in 5e-5
-#          do
-#              TAG="${task}_train_no-demo" \
-#              TYPE=prompt \
-#              TASK="${task}" \
-#              BS=$bs \
-#              LR=$lr \
-#              SEED=$seed \
-#              MODEL=FacebookAI/xlm-roberta-large \
-#              bash run_experiment_train.sh "--mapping_path final_label_mapping/farstail/16-$seed.sort.txt --mapping_id 0"
-#              sleep 180s
-#          done
-#      done
-#  done
-#done
 for task in farstail
 do
   for seed in 13 21 42 87 100
   do
       for bs in 8
       do
-          for lr in 5e-5
+          for lr in 1e-5 2e-5
+          do
+              TAG="${task}_train_no-demo" \
+              TYPE=prompt \
+              TASK="${task}" \
+              BS=$bs \
+              LR=$lr \
+              SEED=$seed \
+              MODEL=FacebookAI/xlm-roberta-large \
+              bash run_experiment_train.sh "--mapping_path final_label_mapping/farstail/16-$seed.sort.txt --mapping_id 0"
+              sleep 180s
+          done
+      done
+  done
+done
+for task in farstail
+do
+  for seed in 13 21 42 87 100
+  do
+      for bs in 8
+      do
+          for lr in 1e-5 2e-5
           do
               TAG="${task}_train_demo" \
               TYPE=prompt-demo \
