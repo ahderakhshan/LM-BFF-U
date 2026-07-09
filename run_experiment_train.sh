2fa028f97bd630c6c1617bc7d9287734a9fe54bc
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-07-09 19:34:47 +0430

    debug

--- run_experiment_train.sh
+++ run_experiment_train.sh
@@ -11,10 +11,10 @@
 K=16
 
 # Training steps
-MAX_STEP=1000
+MAX_STEP=100
 
 # Validation steps
-EVAL_STEP=100
+EVAL_STEP=50
 
 # Task specific parameters
 # The default length is 128 and the default number of samples is 16.
