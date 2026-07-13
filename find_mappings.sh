1a24cbd71ffc8a25fcb555c5a9b560d1713142bb
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-07-13 22:14:53 +0430

    find mapping farexstance

--- find_mappings.sh
+++ find_mappings.sh
@@ -1,25 +1,25 @@
 counter=0
-for mapping_id in 6 17
+for mapping_id in {0..19}
 do
-    for seed in 21
+    for seed in 13 21 42 87 100
     do
-#        ((counter++))
-#        if [ "$counter" -le 64 ]; then
-#            echo "paased ${counter}"
-#            continue
-#        fi
         # To save time, we fix these hyper-parameters
         bs=8
         lr=1e-5
 
         # Since we only use dev performance here, use --no_predict to skip testing
-        TAG=exp-mapping-farstail \
+        TAG=exp-mapping-farexstance \
         TYPE=prompt \
-        TASK=farstail \
+        TASK=farexstance \
         BS=$bs \
         LR=$lr \
         SEED=$seed \
         MODEL=xlm-roberta-large \
-        bash run_experiments_find_mappings.sh "--mapping_path my_auto_label_mapping/manual_template/farstail/16-$seed.txt --mapping_id $mapping_id --no_predict"
+        bash run_experiments_find_mappings.sh "--mapping_path my_auto_label_mapping/manual_template/farexstance/16-$seed.txt --mapping_id $mapping_id --no_predict"
+        ((counter++))
+        sleep 150s
+        if (( counter % 10 == 0 )); then
+          sleep 900
+        fi
     done
 done
