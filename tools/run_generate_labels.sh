67c650a5bfd5b799769e8d3b7346e2305bc5a533
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-07-17 19:39:15 +0430

    change settings

--- tools/run_generate_labels.sh
+++ tools/run_generate_labels.sh
@@ -21,7 +21,7 @@ TEMPLATE_DIR="auto_template/"
 NUM_TEMPLATES=10
 
 # Filter options to top K words (conditional) per class.
-K_LIKELY=20
+K_LIKELY=10
 
 # Special case: we may need to further re-rank based on K-NN.
 K_NEIGHBORS=30
