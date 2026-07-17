4bb517188c2b001684255a764e34f6930d56dbf1
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-07-17 21:03:28 +0430

    check demo filter

--- task_train_demo_filter.sh
+++ task_train_demo_filter.sh
@@ -1,4 +1,4 @@
-#python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task farstail
+python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task farexstance
 #python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task farstail --do_test --seed 42
 #for seed in 13 21 87 100
 #do
@@ -8,43 +8,43 @@
 #    done
 #done
 #counter=0
-for task in farstail
-do
-  for seed in 13
-  do
-      for bs in 8
-      do
-          for lr in 1e-5
-          do
-              TAG="${task}_train_demo-filter" \
-              TYPE=prompt-demo \
-              TASK="${task}" \
-              BS=$bs \
-              LR=$lr \
-              SEED=$seed \
-              MODEL=FacebookAI/xlm-roberta-large \
-              bash run_experiment_train_demo-filter.sh "--mapping_path final_label_mapping/farstail/16-$seed.sort.txt --mapping_id 0"
-          done
-      done
-  done
-done
-for task in farstail
-do
-  for seed in 100
-  do
-      for bs in 4
-      do
-          for lr in 2e-5
-          do
-              TAG="${task}_train_demo-filter" \
-              TYPE=prompt-demo \
-              TASK="${task}" \
-              BS=$bs \
-              LR=$lr \
-              SEED=$seed \
-              MODEL=FacebookAI/xlm-roberta-large \
-              bash run_experiment_train_demo-filter.sh "--mapping_path final_label_mapping/farstail/16-$seed.sort.txt --mapping_id 0"
-          done
-      done
-  done
-done
+#for task in farstail
+#do
+#  for seed in 13
+#  do
+#      for bs in 8
+#      do
+#          for lr in 1e-5
+#          do
+#              TAG="${task}_train_demo-filter" \
+#              TYPE=prompt-demo \
+#              TASK="${task}" \
+#              BS=$bs \
+#              LR=$lr \
+#              SEED=$seed \
+#              MODEL=FacebookAI/xlm-roberta-large \
+#              bash run_experiment_train_demo-filter.sh "--mapping_path final_label_mapping/farstail/16-$seed.sort.txt --mapping_id 0"
+#          done
+#      done
+#  done
+#done
+#for task in farstail
+#do
+#  for seed in 100
+#  do
+#      for bs in 4
+#      do
+#          for lr in 2e-5
+#          do
+#              TAG="${task}_train_demo-filter" \
+#              TYPE=prompt-demo \
+#              TASK="${task}" \
+#              BS=$bs \
+#              LR=$lr \
+#              SEED=$seed \
+#              MODEL=FacebookAI/xlm-roberta-large \
+#              bash run_experiment_train_demo-filter.sh "--mapping_path final_label_mapping/farstail/16-$seed.sort.txt --mapping_id 0"
+#          done
+#      done
+#  done
+#done
