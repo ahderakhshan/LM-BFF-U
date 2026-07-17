baaa6fb9737ef84099ca12f309288a150a855445
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-07-17 21:09:55 +0430

    farexstance train demo filter exps

--- run_experiment_train_demo-filter.sh
+++ run_experiment_train_demo-filter.sh
@@ -113,7 +113,6 @@ case $TASK in
       ;;
     farexstance)
       TEMPLATE=*cls**sent_1**mask*با*sent_0*است.**sep+*
-      MAPPING="{'agree':'موافق','disagree':'مخالف','unrelated':'جدا','discuss':'مرتبط'}"
       TASK_EXTRA="--max_seq_len 512 --num_sample 16 --demo_filter --demo_filter_model PartAI/Tooka-SBERT-V2-Large"
       ;;
 
--- task_train_demo_filter.sh
+++ task_train_demo_filter.sh
@@ -1,50 +1,30 @@
-python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task farexstance
-#python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task farstail --do_test --seed 42
-#for seed in 13 21 87 100
-#do
-#    for task in farstail
-#    do
-#        cp data/k-shot/$task/16-42/test.npy  data/k-shot/$task/16-$seed/
-#    done
-#done
-#counter=0
-#for task in farstail
-#do
-#  for seed in 13
-#  do
-#      for bs in 8
-#      do
-#          for lr in 1e-5
-#          do
-#              TAG="${task}_train_demo-filter" \
-#              TYPE=prompt-demo \
-#              TASK="${task}" \
-#              BS=$bs \
-#              LR=$lr \
-#              SEED=$seed \
-#              MODEL=FacebookAI/xlm-roberta-large \
-#              bash run_experiment_train_demo-filter.sh "--mapping_path final_label_mapping/farstail/16-$seed.sort.txt --mapping_id 0"
-#          done
-#      done
-#  done
-#done
-#for task in farstail
-#do
-#  for seed in 100
-#  do
-#      for bs in 4
-#      do
-#          for lr in 2e-5
-#          do
-#              TAG="${task}_train_demo-filter" \
-#              TYPE=prompt-demo \
-#              TASK="${task}" \
-#              BS=$bs \
-#              LR=$lr \
-#              SEED=$seed \
-#              MODEL=FacebookAI/xlm-roberta-large \
-#              bash run_experiment_train_demo-filter.sh "--mapping_path final_label_mapping/farstail/16-$seed.sort.txt --mapping_id 0"
-#          done
-#      done
-#  done
-#done
+#python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task farexstance
+python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task farexstance --do_test --seed 42
+for seed in 13 21 87 100
+do
+    for task in farexstance
+    do
+        cp data/k-shot/$task/16-42/test.npy  data/k-shot/$task/16-$seed/
+    done
+done
+counter=0
+for task in farexstance
+do
+  for seed in 13 21 42 87 100
+  do
+      for bs in 2 4 8
+      do
+          for lr in 1e-5 2e-5 5e-5
+          do
+              TAG="${task}_train_demo-filter" \
+              TYPE=prompt-demo \
+              TASK="${task}" \
+              BS=$bs \
+              LR=$lr \
+              SEED=$seed \
+              MODEL=FacebookAI/xlm-roberta-large \
+              bash run_experiment_train_demo-filter.sh "--mapping_path final_label_mapping/farexstance/16-$seed.sort.txt --mapping_id 0"
+          done
+      done
+  done
+done
