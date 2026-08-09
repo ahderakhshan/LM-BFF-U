f5fe384e31ad2ea73dc0f223df7911f3c0a5db5e
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-08-09 12:16:22 +0430

    run exp demo filter sentipers multiclass

--- task_train_demo_filter.sh
+++ task_train_demo_filter.sh
@@ -1,3 +1,4 @@
+sleep 6h
 python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task sentipers_multiclass
 python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task sentipers_multiclass --do_test --seed 42
 for seed in 13 21 87 100
@@ -16,11 +17,6 @@ do
       do
           for lr in 1e-5 2e-5 5e-5
           do
-#              if ((counter <= 25)); then
-#                echo "$counter continued"
-#                ((counter++))
-#                continue
-#              fi
               TAG="${task}_train_demo-filter" \
               TYPE=prompt-demo \
               TASK="${task}" \
@@ -29,11 +25,11 @@ do
               SEED=$seed \
               MODEL=FacebookAI/xlm-roberta-large \
               bash run_experiment_train_demo-filter.sh "--mapping_path final_label_mapping/sentipers_multiclass/16-$seed.sort.txt --mapping_id 0"
-#              sleep 120s
-#              ((counter++))
-#              if (( counter % 10 == 0 )); then
-#                sleep 900
-#              fi
+              sleep 120s
+              ((counter++))
+              if (( counter % 10 == 0 )); then
+                sleep 600s
+              fi
           done
       done
   done
