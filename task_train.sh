6e82d60fc1b30ef3900f484870a27e8f54d8ec30
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-08-11 01:17:49 +0430

    continue exps

--- task_train.sh
+++ task_train.sh
@@ -1,15 +1,52 @@
 #
+sleep 2h
+counter=0
+for task in farstail
+do
+  for seed in 13 21 42 87 100
+  do
+      for bs in 2 4 8
+      do
+          for lr in 1e-5 2e-5 4e-5
+          do
+              if ((counter >= 25)); then
+                ((counter++))
+                continue
+              fi
+              TAG="${task}_train_no-demo1" \
+              TYPE=prompt \
+              TASK="${task}" \
+              BS=$bs \
+              LR=$lr \
+              SEED=$seed \
+              MODEL=FacebookAI/xlm-roberta-large \
+              bash run_experiment_train.sh "--mapping_path final_label_mapping/farstail/16-$seed.sort.txt --mapping_id 0"
+              sleep 120s
+              ((counter++))
+              if (( counter % 10 == 0 )); then
+                sleep 900
+              fi
+          done
+      done
+  done
+done
 #counter=0
+##sleep 7200s
 #for task in sentipers_multiclass
 #do
 #  for seed in 13 21 42 87 100
 #  do
 #      for bs in 2 4 8
 #      do
-#          for lr in 1e-5 2e-5 4e-5
+#          for lr in 1e-5 2e-5 5e-5
 #          do
-#              TAG="${task}_train_no-demo" \
-#              TYPE=prompt \
+#              if (( counter <= 21 )); then
+#                echo "$counter passed"
+#                ((counter++))
+#                continue
+#              fi
+#              TAG="${task}_train_demo" \
+#              TYPE=prompt-demo \
 #              TASK="${task}" \
 #              BS=$bs \
 #              LR=$lr \
@@ -19,41 +56,9 @@
 #              sleep 120s
 #              ((counter++))
 #              if (( counter % 10 == 0 )); then
-#                sleep 900
+#                sleep 600s
 #              fi
 #          done
 #      done
 #  done
 #done
-counter=0
-#sleep 7200s
-for task in sentipers_multiclass
-do
-  for seed in 13 21 42 87 100
-  do
-      for bs in 2 4 8
-      do
-          for lr in 1e-5 2e-5 5e-5
-          do
-              if (( counter <= 21 )); then
-                echo "$counter passed"
-                ((counter++))
-                continue
-              fi
-              TAG="${task}_train_demo" \
-              TYPE=prompt-demo \
-              TASK="${task}" \
-              BS=$bs \
-              LR=$lr \
-              SEED=$seed \
-              MODEL=FacebookAI/xlm-roberta-large \
-              bash run_experiment_train.sh "--mapping_path final_label_mapping/sentipers_multiclass/16-$seed.sort.txt --mapping_id 0"
-              sleep 120s
-              ((counter++))
-              if (( counter % 10 == 0 )); then
-                sleep 600s
-              fi
-          done
-      done
-  done
-done
