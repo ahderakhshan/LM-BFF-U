07ed2c2a35970a3b6ef68625324916efdd956a6a
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-08-11 01:19:42 +0430

    fix

--- task_train_demo_filter.sh
+++ task_train_demo_filter.sh
@@ -1,13 +1,13 @@
-sleep 6h
-python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task sentipers_multiclass
-python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task sentipers_multiclass --do_test --seed 42
-for seed in 13 21 87 100
-do
-    for task in sentipers_multiclass
-    do
-        cp data/k-shot/$task/16-42/test.npy  data/k-shot/$task/16-$seed/
-    done
-done
+#sleep 6h
+#python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task sentipers_multiclass
+#python tools/get_sbert_embedding.py --sbert_model "PartAI/Tooka-SBERT-V2-Large" --task sentipers_multiclass --do_test --seed 42
+#for seed in 13 21 87 100
+#do
+#    for task in sentipers_multiclass
+#    do
+#        cp data/k-shot/$task/16-42/test.npy  data/k-shot/$task/16-$seed/
+#    done
+#done
 counter=0
 for task in sentipers_multiclass
 do
