262489efcbe6132369f6d6b0c0cfcbfd118843ca
amirhossien derakhshan <ahderakhshan.ce@gmail.com>
2026-08-22 01:15:22 +0430

    change temp

--- run_experiment_no_train.sh
+++ run_experiment_no_train.sh
@@ -109,7 +109,7 @@ case $TASK in
       TASK_EXTRA="--max_seq_len 512 --num_sample 16"
       ;;
     parsinlu-food-sentiment)
-      TEMPLATE=*cls**sent_0**mask*بود.*sep+*
+      TEMPLATE=*cls**sent_0**mask**sep+*
       MAPPING="{'Positive':'خوب','Negative':'بد','Neutral':'متوسط'}"
       TASK_EXTRA="--max_seq_len 512 --num_sample 16"
       ;;
